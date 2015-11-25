1734ab9bf66a0b71dc57a071c721b8efc7f2e908
duhsokola <duhsokola@gmail.com>
2015-11-25 09:50:13 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,51 @@
-var gulp = require('gulp'),
-  debug = require('gulp-debug');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
 
-/**
- * Combined Tasks
- */
-gulp.task('serve', ['inject', 'browserSync']);
-gulp.task('inject', ['injectBower', 'injectSources']);
+var paths = {
+  sass: ['./scss/**/*.scss']
+};
 
+gulp.task('default', ['sass']);
 
-
-/**
- * Dependency Injection
- */
-var inject = require('gulp-inject'),
-  wiredep = require('wiredep');
-
-gulp.task('injectBower', function () {
-  wiredep({
-      src: './www/index.html',
-      directory: './www/externals/bower_components/',
-      bowerJson: require('./bower.json'),
-      devDependencies: false
-    }
-  )
-});
-gulp.task('injectSources', function () {
-  var target = gulp.src('www/index.html');
-
-  return target.pipe(inject(gulp.src(
-    [
-      'app/**/*.js',
-      'app/**/*.css',
-      'assets/**/*.js',
-      'assets/**/*.css',
-      '!externals/bower_components/**/*.css',
-      '!externals/bower_components/**/*.js'
-    ],
-    {
-      read: false,
-      cwd: 'www'
-    })
-  )).pipe(gulp.dest('./www'));
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
 });
 
-/**
- * Server with BrowserSync
- */
-var browserSync = require('browser-sync'),
-  reload = browserSync.reload;
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
 
-gulp.task('browserSync', function () {
-  browserSync({
-    server: {
-      baseDir: 'www'
-    }
-  });
-  gulp.watch(
-    [
-      'app/**/*.js',
-      'app/**/*.html',
-      'assets/**/*.js',
-      'assets/**/*.css',
-      'externals/**/*.js',
-      'externals/**/*.html',
-      '*.html',
-      'assets/i18n/*.json'
-    ],
-    {
-      cwd: 'www'
-    },
-    reload);
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
 });
